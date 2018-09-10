0773d54ac0ce57ee5d72eed7753812aa60d8dd88
Matthew Hall <matthewhall@Matthews-MacBook-Pro-2.local>
2018-09-10 17:56:05 -0500

    replace jquery with focus

--- js/touch.js
+++ js/touch.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   $('.feature-overlay').mouseover(function() {
     $( this ).css('opacity', '1');
+    if ($('.feature-overlay').css('opacity') ==='1') {
+      $('.feature-overlay').css('opacity', '0');
+    }
   });
-  $('.feature-overlay').mouseout(function() {
-    $( this ).css('opacity', '0');
-  });
-  
+  // $('.feature-overlay').mouseout(function() {
+  //   $( this ).css('opacity', '0');
+  // });
+
   $('.feature-overlay').on('touchstart', function() {
     $( this ).css('opacity', '1');
   });
-  $('.feature-overlay').on('touchend', function() {
-    $( this ).css('opacity', '0');
-  });
+  // $('.feature-overlay').on('touchend', function() {
+  //   $( this ).css('opacity', '0');
+  // });
 });
